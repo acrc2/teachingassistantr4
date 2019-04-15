f0825b263c1e19d279cd68ecceac4ffbbcaaf982
Arthur de Castro Ribeiro Cabeca <acrc2@ug2c23.windows.cin.ufpe.br>
2019-04-15 15:49:35 -0300

    atualização

--- gui/ta-gui/src/app/aluno.service.ts
+++ gui/ta-gui/src/app/aluno.service.ts
@@ -1,8 +1,13 @@
+import { Injectable } from '@angular/core';
+
 import { Aluno } from './aluno';
 
+@Injectable()
 export class AlunoService {
   alunos: Aluno[] = [];
-  gravar(aluno: Aluno): Aluno {
+
+  criar(aluno: Aluno): Aluno {
+    aluno = aluno.clone();
     var result = null;
     if (this.cpfNaoCadastrado(aluno.cpf)) {
       this.alunos.push(aluno);
@@ -10,7 +15,25 @@ export class AlunoService {
     }
     return result;
   }
+
   cpfNaoCadastrado(cpf: string): boolean {
      return !this.alunos.find(a => a.cpf == cpf);
+  }
+
+  atualizar(aluno: Aluno): void {
+    aluno = aluno.clone();
+    for (let a of this.alunos) {
+        if (a.cpf == aluno.cpf) {
+           a.metas = aluno.metas;
+        }
+    }
+  }
+
+  getAlunos(): Aluno[] {
+    var result: Aluno[] = [];
+    for (let a of this.alunos) {
+      result.push(a.clone());
+    }
+    return result;
   }
 }
